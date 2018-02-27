c82c6f0288b70c23d4e00dbc5184c01aa9e1dde5
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2018-02-27 14:40:44 -0600

    cleanup

--- scripts/start-mesos-agents.sh
+++ scripts/start-mesos-agents.sh
@@ -1,11 +1,18 @@
 sudo true
+demo_cmd="sudo /sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --work_dir=/home/rdilley/mesos/DEMO_PORT/ --port=DEMO_PORT --containerizers=docker --docker_remove_delay=1min --resources=agent.conf &"
+
 port=5051
-sudo /sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --work_dir=/home/rdilley/mesos/${port}/ --port=${port} --containerizers=docker --resources=file:///home/rdilley/agent.conf &
+cmd=`echo ${demo_cmd} | sed -e "s/DEMO_PORT/${port}/g"`
+${cmd}
 port=5052
-sudo /sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --work_dir=/home/rdilley/mesos/${port}/ --port=${port} --containerizers=docker --resources=file:///home/rdilley/agent.conf &
+cmd=`echo ${demo_cmd} | sed -e "s/DEMO_PORT/${port}/g"`
+${cmd}
 port=5053
-sudo /sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --work_dir=/home/rdilley/mesos/${port}/ --port=${port} --containerizers=docker --resources=file:///home/rdilley/agent.conf &
+cmd=`echo ${demo_cmd} | sed -e "s/DEMO_PORT/${port}/g"`
+${cmd}
 port=5054
-sudo /sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --work_dir=/home/rdilley/mesos/${port}/ --port=${port} --containerizers=docker --resources=file:///home/rdilley/agent.conf &
+cmd=`echo ${demo_cmd} | sed -e "s/DEMO_PORT/${port}/g"`
+${cmd}
 port=5055
-sudo /sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --work_dir=/home/rdilley/mesos/${port}/ --port=${port} --containerizers=docker --resources=file:///home/rdilley/agent.conf &
+cmd=`echo ${demo_cmd} | sed -e "s/DEMO_PORT/${port}/g"`
+${cmd}
