b8438da6ba91a04b62ef861931fe18ecf155365a
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2018-03-01 16:38:43 -0600

    Adding

---
+++ scripts/start-all.sh
@@ -0,0 +1,7 @@
+./start-message-service.sh
+
+./start-mesos-master.sh
+
+./start-mesos-agents.sh
+
+./start-marathon.sh
---
+++ scripts/stop-all.sh
@@ -0,0 +1,7 @@
+./stop-marathon.sh
+
+./stop-mesos-agents.sh
+
+./stop-mesos-master.sh
+
+./stop-message-service.sh
