d5851cdbe30608d99e93dc6de99c9748d763da9d
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2018-03-02 12:17:27 -0600

    Enhanced all the environment stuff

---
+++ scripts/setup-environment.sh
@@ -0,0 +1,15 @@
+
+# Rabbit
+source environment/rabbitmq
+
+# Message Service
+source environment/message-service
+
+# Docker
+source environment/docker
+
+# Mesos
+source environment/mesos
+
+# Worker
+source environment/worker
